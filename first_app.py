8c242dfb00ecb48d85309d277a9f1b48b5729a13
siamahmed-user <66516210+siamahmed-user@users.noreply.github.com>
2020-11-15 11:50:55 +0600

    Create first_app.py

---
+++ first_app.py
@@ -0,0 +1,19 @@
+print("Hello world")
+#In python print statement is used to print data
+#take a variable
+x = "hello"
+y = "world"
+
+print(x + y)
+#using python to add strings
+
+#we can also use  python as a  calculator
+c = 12
+v = 24
+
+print(c * v)
+#* is used to mulification
+#there are other oparators.
+#this is a messy first time code script just to confuse you XD
+#but we have coded our first script
+#after is we will start our basic  python course
